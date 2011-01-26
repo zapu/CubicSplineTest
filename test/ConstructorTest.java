c5b18badde152fbaacbd6aa0d5cb1fe2e881a175
Michal Zochniak <michal@zapu.net>
2011-01-26 08:07:42 +0100

    Renamed tests. Code Coverage

--- test/ConstructorTest.java
+++ test/ConstructorTest.java
@@ -7,28 +7,35 @@ public class ConstructorTest {
     public static final double eps = 100 * Double.MIN_VALUE;
 
     @Test (expected = ArrayIndexOutOfBoundsException.class)
-    public void test01() {
+    public void emptyArray() {
         double[] x = new double[] { };
         double[] y = new double[] { };
         CubicSplineFast csf = new CubicSplineFast(x, y);
     }
 
     @Test (expected = ArrayIndexOutOfBoundsException.class)
-    public void test02() {
+    public void oneElementArray() {
         double[] x = new double[] { 1 };
         double[] y = new double[] { 2 };
         CubicSplineFast csf = new CubicSplineFast(x, y);
     }
 
     @Test
-    public void test03() {
+    public void twoElementArray() {
         double[] x = new double[] { 1, 3 };
         double[] y = new double[] { 2, 1 };
         CubicSplineFast csf = new CubicSplineFast(x, y);
     }
 
+    @Test
+    public void normalArray() {
+        double[] x = new double[] { 1, 2, 3, 4, 5 };
+        double[] y = new double[] { 2, 1, 2, 1, 0 };
+        CubicSplineFast csf = new CubicSplineFast(x, y);
+    }
+
     @Test (expected = ArrayIndexOutOfBoundsException.class)
-    public void test04() {
+    public void differenyArrays() {
         double[] x = new double[] { 1, 2, 3, 4 };
         double[] y = new double[] { 2, 1, 3 };
         CubicSplineFast csf = new CubicSplineFast(x, y);
